19c8ca9b16dcef851b8e35ad7885a85c05409ebc
Andrey A <aasfaganov@twilio.com>
2021-12-16 23:10:35 +0100

    working realtime update

--- src/App.js
+++ src/App.js
@@ -5,30 +5,32 @@ import "./App.css";
 var {SyncClient} = require("twilio-sync");
 
 function App() {
-  useEffect(async ()=>{
-    getAccessToken()
-    // .then(()=>{
-    //   getTx();
-    // })
-    // .catch((e)=>{
-    //   console.log(e);
-    // })
-  },[]);
-
-
   const [token, setToken] = useState('');
   const [syncClient, setSyncClient] = useState(null);
   const [inT, setIncomingList] = useState([]);
   const [outT, setOutgoingList] = useState([]);
 
-  async function getAccessToken() {
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/token", { method: "POST" })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       const syncClient = new SyncClient(data.token);
+  //       syncClient.list("incoming").then((list) => {
+  //         list.getItems().then((page) => {
+  //           setIncomingList(page.items.map((item) => item));
+  //         });
+  //         list.on("itemAdded", (e) => {
+  //           console.log(e);
+  //           setIncomingList(inT => [...inT, e.item]);
+  //         });
+  //       });
+  //     });
+  // }, []);
 
-    const res = await fetch(`http://localhost:3000/token`);
-    const data = await res.json();
-    console.log(data.token);
-    var syncClient = new SyncClient(data.token);
 
-    setToken(data.token);
+  useEffect(async ()=>{
+    let token = await getAccessToken();
+    var syncClient = new SyncClient(token);
     setSyncClient(syncClient);
     const incomingList = await syncClient.list('incoming');
     const outgoingList = await syncClient.list('outgoing');
@@ -42,10 +44,8 @@ function App() {
     incomingList.on('itemAdded', event => {
       if (!event.isLocal) {
         console.log("item added", event);
-        let newInT = inT.items;
-        newInT.push(event.item);
-        console.log(newInT);
-        setIncomingList(newInT);
+
+        setIncomingList([...inT.items, event.item]);
       }
     });
 
@@ -54,7 +54,24 @@ function App() {
         console.log("item removed", event);
       }
     });
-    // console.log(inT, outT);  
+
+  },[]);
+
+
+  async function getAccessToken() {
+
+    const res = await fetch(`http://localhost:3000/token`);
+    const data = await res.json();
+    console.log(data.token);
+    setToken(data.token);
+    return data.token;
+    
+  }
+
+  async function getTx(token){
+    
+
+
   }
 
 
@@ -65,6 +82,7 @@ function App() {
       <header className="App-header">
         <h1>Bucket #1: donation counter</h1>
         <div className="bucket">
+          {inT.length}
           <div className="bucket-inT">
             {inT.map(i => (
               <div className="inT" style={{height:`${i.data.amount*2}px`}} key={i.uri}> {i.data.amount}</div>
